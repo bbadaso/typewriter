27399269de74c6e6a85ea7979dd6b3666c5e995f
bbadaso <badasobarento@gmail.com>
2021-12-04 04:17:49 +0000

    typewriter

---
+++ typewriter.js
@@ -0,0 +1,16 @@
+const sentence = "hello there from lighthouse labs"; 
+
+let delay = 50;
+for (const char of sentence) {
+
+  setTimeout(() => {
+
+    process.stdout.write(char);
+}, delay)
+delay = delay + 50
+}
+
+setTimeout(() => {
+  console.log("\n")
+}, 1850)
+
